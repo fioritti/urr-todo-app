b1e22f425bb9293af497ed8df0f86a0a51440a71
Unknown <guileme13@gmail.com>
2018-10-17 08:03:34 -0300

    Integrando projeto com redux

--- frontend-wrx/src/index.jsx
+++ frontend-wrx/src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import multi from 'redux-multi';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
 import App from './main/app';
 import reducers from './main/reducers';
 
-
-const store = createStore(reducers)
-
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
